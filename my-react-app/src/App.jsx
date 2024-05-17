42c403109bc7da77beab6e7fec829ea3ad48f606
Patience <patiencerasebitse@gmail.com>
2024-05-17 17:13:40 +0200

    Installed React Router Dom.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
-import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Blog from './Pages/Blog';
+import Donations from './Pages/Donations';
 
 function App() {
 
   return (
-      <div>
-<h1>Church Project</h1>
-        <Navbar/>
+      <div className="App">
+        <Routes>
+         <Route path='/' element={<Home/>} />
+         <Route path='/about' element={<About/>} />
+         <Route path='/contact' element={<Contact/>} />
+         <Route path='/blog' element={<Blog/>} />
+         <Route path='/Donations' element={<Donations/>} />
+       </Routes>
+
       </div>
   )
 }
