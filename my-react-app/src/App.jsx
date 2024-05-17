3cb011b95476266222bd2a1c69f150bb4a2df3fe
Patience <patiencerasebitse@gmail.com>
2024-05-17 14:56:57 +0200

    Removed all unused lines of code.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,21 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './Navbar'
-import Content from './Content'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
       <div>
+<h1>Church Project</h1>
         <Navbar/>
       </div>
-      <h1>Church Project</h1>
-      <Content/>
-    </>
   )
 }
 
