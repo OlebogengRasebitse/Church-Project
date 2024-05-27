da6e235604e5ccb6194cc6942df3bfe4c212b092
Patience <patiencerasebitse@gmail.com>
2024-05-27 19:07:43 +0200

    Installed bootstrap and created a new navbar.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,11 +5,14 @@ import About from './Pages/About';
 import Contact from './Pages/Contact';
 import Blog from './Pages/Blog';
 import Donations from './Pages/Donations';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CustomNavbar from './components/CustomNavbar';
 
 function App() {
 
   return (
-      <div className="App">
+      <div className="container mt-4">
+           <CustomNavbar />
         <Routes>
          <Route path='/' element={<Home/>} />
          <Route path='/about' element={<About/>} />
