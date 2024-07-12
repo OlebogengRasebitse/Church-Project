40f992d37ba64c1894dd3b282714772f528dc505
Patience <patiencerasebitse@gmail.com>
2024-07-12 10:16:59 +0200

    Added content to homepage and removed color.

--- my-react-app/src/Pages/Home/Home.jsx
+++ my-react-app/src/Pages/Home/Home.jsx
@@ -4,7 +4,8 @@ import "./Home.css"
 const Home = () => {
     return (
         <div className="home-container">
-            <h3>Home</h3>
+            <h3>About our church</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, sit tenetur! Provident optio aperiam non laudantium autem et officia dolorum itaque magnam totam fuga sint tempora commodi, officiis nobis quam. </p>
         </div>
     );
 }
