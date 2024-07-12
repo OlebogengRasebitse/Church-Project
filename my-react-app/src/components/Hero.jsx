60e4f58dda0f6a064a69917b790271976ac30ad0
Patience <patiencerasebitse@gmail.com>
2024-07-12 10:18:17 +0200

    Added hero description

--- my-react-app/src/components/Hero.jsx
+++ my-react-app/src/components/Hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
         <div className="hero-container">
                   <img src={heroImage} alt="Hero" className="hero-image" />
             <div className="hero-content">
-                <h1>Your Hero Title</h1>
-                <p>Your Hero Subtitle or Description</p>
+                <h1>Title</h1>
+                <p>"Let all that you do be done in love." - 1 Corinthians 16:14</p>
             </div>
         </div>
     );
