987dc6512184b4c17fe4f281f8aa9d0960c643a0
Patience <patiencerasebitse@gmail.com>
2024-07-26 14:10:34 +0200

    Changed Hero Image and brightness

--- my-react-app/src/components/Hero.jsx
+++ my-react-app/src/components/Hero.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import './Hero.css';
-import heroImage from '../images/Hero.jpg';
+import heroImage from '../images/NewHero.jpg';
 
 const Hero = () => {
     return (
         <div className="hero-container">
                   <img src={heroImage} alt="Hero" className="hero-image" />
             <div className="hero-content">
-                <h1>Title</h1>
+                <h1><strong>Church Great Commission</strong></h1>
                 <p>"Let all that you do be done in love." - 1 Corinthians 16:14</p>
             </div>
         </div>
