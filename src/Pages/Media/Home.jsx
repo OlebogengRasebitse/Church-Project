b6640d887fa33fabe892151d28fab481fa6c8f34
Patience <patiencerasebitse@gmail.com>
2025-03-17 00:15:21 +0200

    changed Media page
    Added new images and style

---
+++ src/Pages/Media/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css"
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h2>About our church</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, sit tenetur! Provident optio aperiam non laudantium autem et officia dolorum itaque magnam totam fuga sint tempora commodi, officiis nobis quam.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore distinctio obcaecati perspiciatis consequatur quos ex magni sint ipsa officiis! Officiis animi at aspernatur, fugiat unde labore eaque voluptates. Excepturi, rem.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla voluptas id, nemo architecto, sit ea consequatur repellendus quo facere incidunt fugiat consectetur reprehenderit. Repellendus minus quidem est animi explicabo blanditiis?
+            </p>
+        </div>
+    );
+}
+
+export default Home;
